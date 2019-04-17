e7856ae0f339e77026477a37a7fdddad9d5286d1
mhmoudalaskalany <mhmoudalaskalany@gmail.com>
2019-04-17 11:07:22 +0200

    =add interfaces for vehicle and save vehicle

---
+++ src/app/components/vehicle-form/models/vehicle.ts
@@ -0,0 +1,26 @@
+export interface KeyValuePair {
+  id: number;
+  name: string;
+}
+export interface Contact {
+  name: string;
+  phone: string;
+  email: string;
+}
+export interface Vehicle  {
+  id: number;
+  model: KeyValuePair;
+  make: KeyValuePair;
+  contact: Contact;
+  features: KeyValuePair[];
+  isRegistered: boolean;
+  lastUpdate: string;
+}
+export interface SaveVehicle {
+  id: number;
+  modelId: number;
+  makeId: number;
+  isRegistered: boolean;
+  features: number[];
+  contact: Contact;
+}
--- src/app/components/vehicle-form/vehicle-form.component.ts
+++ src/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,7 +1,9 @@
+import * as _ from 'underscore';
 import { Component, OnInit } from '@angular/core';
 import { VehicleService } from 'src/app/services/make/vehicle.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { forkJoin } from 'rxjs';
+import { Vehicle, SaveVehicle } from './models/vehicle';
 @Component({
   selector: 'app-vehicle-form',
   templateUrl: './vehicle-form.component.html',
@@ -12,44 +14,68 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   features: any[];
-  vehicle: any = {
-    features : [],
-    contact : {}
+  vehicle: SaveVehicle = {
+    id: 0,
+    makeId: 0,
+    modelId: 0,
+    features: [],
+    contact: {
+      name: '',
+      phone: '',
+      email: ''
+    },
+    isRegistered: false
+
   };
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private vehicleService: VehicleService,
-    ) {
-      this.route.params.subscribe(p => {
-        this.vehicle.id = +p['id'];
-      });
-    }
+  ) {
+    this.route.params.subscribe(p => {
+      this.vehicle.id = +p['id'];
+    });
+  }
 
   ngOnInit() {
-    const sources = [
-      this.vehicleService.getMakes(),
-      this.vehicleService.getFeatures(),
-      this.vehicleService.getVehicle(this.vehicle.id)
-    ];
-    Observable.forkJoin(sources).subscribe((data) => {
-      this.makes = data[0],
-      this.features = data[1]
-      this.vehicle = data[2]
+    this.getVehicleData().subscribe((data: any) => {
+      this.makes = data[0];
+      this.features = data[1];
+      if (this.vehicle.id) {
+        this.setVehicle(data[2]);
+      }
     }, error => {
-      if (error.status === 404) {
+      if(error.status === 404) {
         this.router.navigate(['/home']);
       }
     });
   }
 
+  private setVehicle(v: Vehicle) {
+    this.vehicle.id = v.id;
+    this.vehicle.makeId = v.make.id;
+    this.vehicle.modelId = v.model.id;
+    this.vehicle.isRegistered = v.isRegistered;
+    this.vehicle.contact = v.contact;
+    this.vehicle.features = _.pluck(v.features , 'id');
+  }
+  getVehicleData() {
+    let sources = [
+      this.vehicleService.getMakes(),
+      this.vehicleService.getFeatures()
+    ];
+    if (this.vehicle.id) {
+      sources.push(this.vehicleService.getVehicle(this.vehicle.id));
+    }
+    return forkJoin(sources);
+  }
   onMakeChange() {
     // tslint:disable-next-line:triple-equals
     const selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
     this.models = selectedMake ? selectedMake.models : [];
     delete this.vehicle.modelId;
   }
-  onFeatureToggle(id , $event) {
+  onFeatureToggle(id, $event) {
     if ($event.target.checked) {
       this.vehicle.features.push(id);
     } else {
